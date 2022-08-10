241ba55666b6d82a88af8da4128a8c546aa86032
Andre Barber <andre.barber0505@gmail.com>
2022-08-10 13:23:57 -0500

    orderSession

--- server/api/index.js
+++ server/api/index.js
@@ -3,7 +3,7 @@ module.exports = router;
 
 router.use("/users", require("./users"));
 router.use("/products", require("./products"));
-router.use("/cart", require("./productOrderSession"));
+router.use("/cart", require("./producOrderSession"));
 
 router.use((req, res, next) => {
   const error = new Error("Not Found");
