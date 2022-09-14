f1d70efd304a10206859c1b037f70b2f589461aa
Kevin Yu <Kevin.Yu.2468@gmail.com>
2022-09-13 18:14:19 -0700

    Update model

--- client/components/Model.js
+++ client/components/Model.js
@@ -3,7 +3,7 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import React from "react";
 
 const Model = () => {
-  const gltf = useLoader(GLTFLoader, "/models/winking_face/scene.gltf");
+  const gltf = useLoader(GLTFLoader, "/models/face_savoring_food/scene.gltf");
   return (
     <>
       <primitive object={gltf.scene} scale={0.4} />
