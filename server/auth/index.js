b9b116d745456e73ae467fea86d0e0eec9497aa5
Kevin Yu <Kevin.Yu.2468@gmail.com>
2022-10-13 18:17:17 -0700

    Add merge cart feature for local cart and database cart

--- server/auth/index.js
+++ server/auth/index.js
@@ -15,7 +15,6 @@ router.post("/login", async (req, res, next) => {
 router.post("/signup", async (req, res, next) => {
   try {
     const user = await User.create(req.body);
-    // user.createOrderSession({ status: "active" });
     res.send({ token: await user.generateToken() });
   } catch (err) {
     if (err.name === "SequelizeUniqueConstraintError") {
