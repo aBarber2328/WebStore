ca3327d8b250f15f5f76000d11401b52dea366e1
Kevin Yu <Kevin.Yu.2468@gmail.com>
2022-10-15 16:42:17 -0700

    Update stripe return URL

--- client/pages/Checkout.js
+++ client/pages/Checkout.js
@@ -19,7 +19,11 @@ const stripePromise = loadStripe(
 const CheckoutForm = () => {
   const stripe = useStripe();
   const elements = useElements();
+  const currentUrl = window.location.href;
+  const return_url =
+    currentUrl.slice(0, currentUrl.lastIndexOf("/")) + "/purchase-success";
 
+  console.log(return_url);
   const [errorMessage, setErrorMessage] = useState(null);
 
   const handleSubmit = async (event) => {
@@ -37,7 +41,7 @@ const CheckoutForm = () => {
       //`Elements` instance that was used to create the Payment Element
       elements,
       confirmParams: {
-        return_url: "http://localhost:8080/purchase-success",
+        return_url: return_url,
       },
     });
 
